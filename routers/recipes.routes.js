f902fc68158841b7b56e05801dbde43a73e717b3
Aaron Whitehead <whitehead.aaron@live.com>
2018-09-06 11:32:05 -0700

    modularized a bit

--- routers/recipes.routes.js
+++ routers/recipes.routes.js
@@ -1,1 +1,63 @@
 'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const {Recipes} = require('../models');
+
+
+const jsonParser = bodyParser.json();
+const router = express.Router();
+
+
+
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients', 'id'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing ${field} in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+    if (req.params.id !== req.body.id) {
+      const message = `The request params id ${req.params.id} does not match the body id ${req.body.id}`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  
+    Recipes.update({
+      id: req.params.id,
+      name: req.body.name,
+      ingredients: req.body.ingredients
+    });
+    console.log(`Updated item ${req.params.id}`);
+    res.status(204).end();
+  }
+});
+
+
+router.get('/', (req, res) => {
+  res.json(Recipes.get());
+});
+
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
+
+router.delete('/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+module.exports = {router};
--- routers/shopping-list.routes.js
+++ routers/shopping-list.routes.js
@@ -1,1 +1,63 @@
 'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ShoppingList} = require('../models');
+
+
+const router = express.Router();
+const jsonParser = bodyParser.json();
+
+
+router.get('/', (req, res) => {
+  res.json(ShoppingList.get());
+});
+
+router.post('/', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'budget'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = ShoppingList.create(req.body.name, req.body.budget);
+  res.status(201).json(item);
+});
+
+
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'budget', 'id'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating shopping list item \`${req.params.id}\``);
+  ShoppingList.update({
+    id: req.params.id,
+    name: req.body.name,
+    budget: req.body.budget
+  });
+  res.status(204).end();
+});
+
+router.delete('/:id', (req, res) => {
+  ShoppingList.delete(req.params.id);
+  console.log(`Deleted shopping list item \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+module.exports = {router};
